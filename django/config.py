ba2a6d296c2ffafd4aa1d9a59cf539ff9acebbc8
2ks22 <lksun0425@gmail.com>
2023-01-16 17:36:36 +0900

    내 주변 정비소 찾기, 정비소 검색 기능 추가

---
+++ django/config.py
@@ -0,0 +1,23 @@
+
+MYSQL_CONFIG = {
+    'NAME': 'project03',
+    'USER': 'sjy01',
+    'PASSWORD': 'asdf1234',
+    'HOST': '13.208.82.150',
+    'PORT': '3306',
+}
+
+MONGODB_CONFIG = {
+    'url': 'mongodb://13.208.82.150:27017'
+}
+
+ELASTIC_CONFIG = {
+    'url': 'http://13.208.82.150:9200'
+}
+
+KAKAO_MAP_KEY = {
+    'appkey': 'dd814cd8c09cb0d6fe824b8ab473ed93'
+}
+
+DJANGO_CONFIG = 'django-insecure-0cyl!o8(i=ulp7_gp&g+h85xr4x_ud&y9^#9$ws(kl6+_=5r&6'
+
